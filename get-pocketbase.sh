27759b8c1719467f5c332b506b3ee6a174a7ee1d
Azure <dygticky@gmail.com>
2024-05-18 15:03:54 +0800

    Update Code

--- get-pocketbase.sh
+++ get-pocketbase.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env sh
 echo "Downloading PocketBase"
 
-wget https://github.com/pocketbase/pocketbase/releases/download/v0.22.12/pocketbase_0.22.12_linux_arm64.zip
+wget -q --show-progress https://github.com/pocketbase/pocketbase/releases/download/v0.22.12/pocketbase_0.22.12_linux_arm64.zip
 
 mkdir pocketbase
 echo "Unzip PocketBase"
-unzip pocketbase_0.22.12_linux_arm64.zip
+unzip pocketbase_0.22.12_linux_arm64.zip -d ./pocketbase
 
 echo "Install PocketBase to /usr/local/bin"
-chmod +x ./pocketbase
-mv ./pocketbase /usr/local/bin
+chmod +x ./pocketbase/pocketbase
+mv ./pocketbase/pocketbase /usr/local/bin
 
 echo "Remove unused files"
-rm ./LICENSE.md
-rm ./CHANGELOG.md
+rm ./pocketbase/LICENSE.md
+rm ./pocketbase/CHANGELOG.md
 rm ./pocketbase_0.22.12_linux_arm64.zip
 
-echo "Making Dictionary 'Agent'"
-
-mkdir pocketbase
 
 echo "Downloading Agent UI"
-wget https://github.com/dmeww/maa-agent-ui/releases/download/pb_release/pb_public.zip
+wget -q --show-progress https://github.com/dmeww/maa-agent-ui/releases/download/pb_release/pb_public.zip
 echo "Unzip Agent UI to ./pocketbase"
 unzip pb_public.zip -d ./pocketbase
 
-cat <<EOF > ecosystem.config.js
-module.exports = {
-  apps : [{
-    script: 'pocketbase serve --http 0.0.0.0:8090 --dir ./pb_data --publicDir ./pb_public',
-    name: 'pocketbase'
-  }]
-};
+cat <<EOF > ./pocketbase/start.sh
+#!/usr/bin/bash
+PB="$0"
+if expr "PB" : '.*/' > /dev/null; then
+  PB="${PB%/*}"
+else
+  PB="./"
+fi
+
+pocketbase serve --http 0.0.0.0:8090 --dir "${PB}/pb_data" --publicDir "${PB}/pb_public"
 EOF
 
-echo "你现在可以使用 pm2 start ./pocketbase/ecosystem.config.js 来启动PocketBase(with AgentUI)了"
+rm ./pb_public.zip
+echo "你现在可以使用 ./pocketbase/start.sh 来启动PocketBase(with AgentUI)了"
